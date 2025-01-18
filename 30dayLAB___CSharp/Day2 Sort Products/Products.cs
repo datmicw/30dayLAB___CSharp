3c5503b340b06376ca53484334652da1a8257126
datmicw <dat.micf@gmail.com>
2025-01-18 23:45:25 +0700

    Day 2 Start

---
+++ 30dayLAB___CSharp/Day2 Sort Products/Products.cs
@@ -0,0 +1,13 @@
+﻿//using System.Runtime.Intrinsics.X86;
+
+//Day 2: Sort Products
+//Task:
+//Create a class Product with properties ID, Name, and Price.
+//• Input a list of products and sort them by price (ascending and descending).
+//• Algorithm: Use Bubble Sort to implement sorting
+namespace _30dayLAB___CSharp.Day2_Sort_Products
+{
+    internal class Products
+    {
+    }
+}
