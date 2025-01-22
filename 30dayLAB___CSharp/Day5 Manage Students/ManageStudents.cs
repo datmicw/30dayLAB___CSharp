492e27c40cc27c9bfb42724afa4e32e8af5dd77c
datmicw <dat.micf@gmail.com>
2025-01-22 09:17:49 +0700

    Day 4: Start

---
+++ 30dayLAB___CSharp/Day5 Manage Students/ManageStudents.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace _30dayLAB___CSharp.Day5_Manage_Students
+{
+    internal class ManageStudents
+    {
+        List<Student> students = new List<Student>();
+        private string baseURL = "C:\\Users\\datmi\\OneDrive\\Documents\\Coder\\30dayLAB___CSharp\\30dayLAB___CSharp\\Data\\Day4_StudentManage.txt";
+        public ManageStudents()
+        {
+            students = new List<Student();
+        }
+    }
+}
---
+++ 30dayLAB___CSharp/Day5 Manage Students/Student.cs
@@ -0,0 +1,15 @@
+﻿//Day 5: Manage Students
+//Task:
+//Create a class Student with properties ID, Name, and Grades.
+//• Calculate the average score of each student.
+//• Find the student with the highest and lowest average scores.
+//• Algorithm: Iterate through the list and compare values.
+namespace _30dayLAB___CSharp.Day5_Manage_Students
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<float> Grade { get; set; } // List of grades
+    }
+}
